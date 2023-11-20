f99102df202addd564bac83cb64d4dc8a995992a
Sven Heiberg <sven@ivotingcentre.ee>
2023-11-20 13:50:27 +0200

    id-card-lib/src/.../idcard: clean comments

--- id-card-lib/src/main/java/ee/ria/DigiDoc/idcard/TokenWithPace.java
+++ id-card-lib/src/main/java/ee/ria/DigiDoc/idcard/TokenWithPace.java
@@ -1,22 +1,3 @@
-/*
- * Copyright 2017 - 2023 Riigi Infosüsteemi Amet
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- */
-
 package ee.ria.DigiDoc.idcard;
 
 import org.bouncycastle.util.encoders.Hex;
@@ -27,7 +8,7 @@ import ee.ria.DigiDoc.smartcardreader.SmartCardReaderException;
 import ee.ria.DigiDoc.smartcardreader.nfc.NfcSmartCardReader;
 
 /**
- * Extended EstEid token interface to create PACE-enabled cards
+ * Extended ID1 token interface to create PACE-enabled cards
  */
 public interface TokenWithPace extends Token {
 
