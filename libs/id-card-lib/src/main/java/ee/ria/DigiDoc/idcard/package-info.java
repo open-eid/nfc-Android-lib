899bb60e397e27e6bda4f33ad0ccf2c353d7587b
Sven Heiberg <sven@ivotingcentre.ee>
2024-01-25 11:17:22 +0200

    libs/id-card-lib: Add package level documentation

---
+++ libs/id-card-lib/src/main/java/ee/ria/DigiDoc/idcard/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * Provides interfaces for different versions of ID-cards, both wired and wireless.
+ * <h1>Public interfaces</h1>
+ *
+ * <h1>Implementation</h1>
+ *
+ */
+package ee.ria.DigiDoc.idcard;
