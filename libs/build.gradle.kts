174b5a0f7ba378166a395e5a9ffcbc820ac5266b
Boriss Melikjan <boriss.melikjan@nortal.com>
2024-10-07 13:45:09 +0300

    MOPPAND-1381 Update dependencies.

--- libs/build.gradle.kts
+++ libs/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.library") version "8.1.4" apply false
 }
 
 val androidxVersion by extra("1.6.0")
@@ -13,9 +13,9 @@ val bouncycastleVersion by extra ("1.70")
 
 val autoValueVersion by extra ("1.10.1")
 
-val guavaVersion by extra ("31.1-android")
-val rxJavaVersion by extra ("3.1.6")
+val guavaVersion by extra ("33.3.1-android")
+val rxJavaVersion by extra ("3.1.8")
 
-val junitVersion by extra ("5.10.0")
-val truthVersion by extra ("1.1.3")
-val mockitoVersion by extra ("5.4.0")
+val junitVersion by extra ("5.10.2")
+val truthVersion by extra ("1.4.2")
+val mockitoVersion by extra ("5.11.0")
