a86d8397e11cdc4085f47213b720030e0107a47c
Boriss Melikjan <borissmelikjan@gmail.com>
2024-10-08 14:38:19 +0300

    Update Gradle Wrapper to 8.9 and AGP to 8.7.0.
    * MOPPAND-1381 Update Gradle Wrapper to 8.9 and AGP to 8.7.0.

--- libs/build.gradle.kts
+++ libs/build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.library") version "8.1.4" apply false
+    id("com.android.library") version "8.7.0" apply false
 }
 
 val androidxVersion by extra("1.6.0")
